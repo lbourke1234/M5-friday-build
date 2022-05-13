93a26037787c6c1c259f7b5808203da469ff4c4e
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-05-13 13:31:57 +0100

    Rasmus - Update

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -1,26 +1,26 @@
-import fs from 'fs-extra'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import fs from "fs-extra";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-const { readJSON, writeFile, writeJSON } = fs
+const { readJSON, writeFile, writeJSON } = fs;
 
-const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), '../data')
+const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data");
 
-const productJSONPath = join(dataFolderPath, 'products.json')
-const reviewsJSONPath = join(dataFolderPath, 'reviews.json')
+const productJSONPath = join(dataFolderPath, "products.json");
+const reviewsJSONPath = join(dataFolderPath, "reviews.json");
 
-const productsPublicFodlerPath = join(process.cwd(), './public/img/products')
-const reviewsPublicFodlerPath = join(process.cwd(), './public/img/reviews')
+const productsPublicFodlerPath = join(process.cwd(), "./public/img/products");
+const reviewsPublicFodlerPath = join(process.cwd(), "./public/img/reviews");
 
 export const getProducts = () => {
-  return readJSON(productJSONPath)
-}
+  return readJSON(productJSONPath);
+};
 export const getReviews = () => {
-  return readJSON(reviewsJSONPath)
-}
+  return readJSON(reviewsJSONPath);
+};
 export const writeProducts = (productsArray) => {
-  return writeJSON(productJSONPath, productsArray)
-}
+  return writeJSON(productJSONPath, productsArray);
+};
 export const writeReviews = (ReviewsArray) => {
-  return writeJSON(productJSONPath, ReviewsArray)
-}
+  return writeJSON(reviewsJSONPath, ReviewsArray);
+};
