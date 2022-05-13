e6800e8b320a4c0419ee6d76d337cd4e4e7493d2
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-05-13 15:13:00 +0100

    PUT

--- src/apis/products/index.js
+++ src/apis/products/index.js
@@ -12,6 +12,7 @@ import {
   checkReviewsSchema,
 } from "./validation.js";
 import uniqid from "uniqid";
+import createError from "http-errors";
 
 const productsRouter = express.Router();
 
@@ -160,5 +161,65 @@ productsRouter.get("/:productId/reviews/:reviewId", async (req, res, next) => {
     next(error);
   }
 });
+
+productsRouter.put("/:productId/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const products = await getProducts();
+    const index = products.findIndex(
+      (product) => product.id === req.params.productId
+    );
+    console.log("INDEX OF  :", index);
+
+    const foundReview = products[index].reviews.find(
+      (review) => review.reviewId === req.params.reviewId
+    );
+
+    if (foundReview) {
+      const oldReview = products[index];
+
+      const updatedReview = {
+        ...oldReview,
+        ...req.body,
+        updatedAt: new Date(),
+      };
+      console.log("INDEX OF old :", updatedReview);
+
+      products[index].reviews = updatedReview;
+      await writeProducts(products);
+      res.send(updatedReview);
+    } else {
+      next(createError(404, "no review ID found"));
+    }
+    console.log("INDEX OF find:", foundReview);
+  } catch (error) {
+    next(error);
+  }
+});
+
+productsRouter.delete(
+  "/:productId/reviews/:reviewId",
+  async (req, res, next) => {
+    try {
+      const products = await getProducts();
+      const foundProduct = products.find(
+        (product) => product.id === req.params.productId
+      );
+      const findReview = foundProduct.reviews.find(
+        (review) => review.reviewId === req.params.reviewId
+      );
+
+      const remainingReview = foundProduct.reviews.filter(
+        (review) => findReview.id !== review.reviews.reviewId
+      );
+
+      console.log("HERE IS THE REVIEW", remainingReview);
+
+      await writeProducts(remainingReview);
+      res.status(204).send();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default productsRouter;
