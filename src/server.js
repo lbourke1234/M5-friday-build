b0ff4c1cccb795c9028d8df2008dfa58a8e69705
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-05-17 14:13:48 +0100

    adding files

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import productsRouter from "./apis/products/index.js";
+import cors from "cors";
+import { join } from "path";
+
+const server = express();
+
+const port = process.env.PORT | 5001;
+
+server.use(express.json());
+
+server.use(cors());
+
+server.use("/products", productsRouter);
+
+const publicFolderPath = join(process.cwd(), "./public");
+
+server.use(express.static(publicFolderPath));
+
+server.listen(port, () => {
+  console.table(listEndpoints(server));
+  console.log(`Server is listening on port -->  ${port}`);
+});
+
+server.on("error", (error) => {
+  console.log("new error", error);
+});
